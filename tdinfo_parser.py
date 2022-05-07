79daed148b65cbc30bc65042b50c56cf771b58f3
ramikg <72725910+ramikg@users.noreply.github.com>
2022-05-07 18:06:31 +0300

    Rename methods

--- tdinfo_parser.py
+++ tdinfo_parser.py
@@ -41,13 +41,12 @@ class TdinfoParser(object):
 
         return parsed_exe
 
-    def apply_tdinfo(self):
+    def apply(self):
         applied_symbols_count = 0
         already_existing_symbols_count = 0
         for symbol in self._parsed_exe.symbol_records:
             try:
-                self._apply_tdinfo_symbol(symbol)
-                self._apply_tdinfo_type(symbol)
+                self._apply_symbol(symbol)
                 applied_symbols_count += 1
             except TdinfoParserSymbolAlreadyAppliedException:
                 already_existing_symbols_count += 1
@@ -55,8 +54,8 @@ class TdinfoParser(object):
                 pass
 
         for segment in self._parsed_exe.segment_records:
-            self._apply_tdinfo_segment(segment)
-            self._apply_tdinfo_scopes(segment)
+            self._apply_segment(segment)
+            self._apply_scopes(segment)
 
         print('Detected {} global symbols.'.format(
             self._parsed_exe.tdinfo_header.globals_count)),
@@ -64,7 +63,7 @@ class TdinfoParser(object):
             already_existing_symbols_count,
             applied_symbols_count))
 
-    def _apply_tdinfo_symbol(self, symbol):
+    def _apply_symbol(self, symbol):
         if symbol.bitfield.symbol_class != tdinfo_structs.SymbolClass.STATIC.name:
             raise TdinfoParserUnsupportedSymbolClassException()
 
@@ -82,7 +81,7 @@ class TdinfoParser(object):
         else:
             raise TdinfoParserIdaSetNameFailedException()
 
-    def _apply_tdinfo_type(self, symbol):
+    def _apply_type(self, symbol):
         if (symbol.bitfield.symbol_class != tdinfo_structs.SymbolClass.STATIC.name or
             symbol.type == 0):
             return
@@ -91,9 +90,9 @@ class TdinfoParser(object):
         symbol_name = str(self._parsed_exe.name_pool[symbol.index - 1])
 
         type = self._parsed_exe.type_records[symbol.type - 1]
-        self._apply_tdinfo_type_rec(symbol_ea, symbol_name, type)
+        self._apply_type_record(symbol_ea, symbol_name, type)
 
-    def _apply_tdinfo_type_rec(self, symbol_ea, symbol_name, type):
+    def _apply_type_record(self, symbol_ea, symbol_name, type):
         if (type.id == tdinfo_structs.TypeId.SCHAR.name or
             type.id == tdinfo_structs.TypeId.UCHAR.name):
             idc.create_byte(symbol_ea)
@@ -109,15 +108,15 @@ class TdinfoParser(object):
             if member.id == tdinfo_structs.TypeId.ARRAY.name: # array of arrays
                 idc.make_array(symbol_ea, type.size)
             else:
-                self._apply_tdinfo_type_rec(symbol_ea, symbol_name, member)
+                self._apply_type_record(symbol_ea, symbol_name, member)
                 idc.make_array(symbol_ea, type.size // member.size)
         elif type.id == tdinfo_structs.TypeId.STRUCT.name:
             struct_name = 'struct' + symbol_name
             if get_struc_id(struct_name) == BADADDR: #check if struct already exists
-                self._apply_tdinfo_struct(struct_name, type)
+                self._apply_struct(struct_name, type)
             idc.create_struct(symbol_ea, -1, struct_name)
 
-    def _apply_tdinfo_struct(self, struct_name, type): #create struct + members
+    def _apply_struct(self, struct_name, type): #create struct + members
         id = idc.add_struc(-1, struct_name, 0)
 
         memberIndex = type.member_type - 1
@@ -143,7 +142,7 @@ class TdinfoParser(object):
             if member.info == 0xC0: #end marker
                 break
 
-    def _apply_tdinfo_segment(self, segment):
+    def _apply_segment(self, segment):
         segment_ea = self._image_base + segment.code_segment * 0x10 + segment.code_offset
         module = self._parsed_exe.module_records[segment.module - 1]
         module_name = str(self._parsed_exe.name_pool[module.name - 1])
@@ -153,20 +152,20 @@ class TdinfoParser(object):
                 module_name,
                 self._image_base // 0x10 + segment.code_segment, segment.code_offset))
 
-    def _apply_tdinfo_scopes(self, segment):
+    def _apply_scopes(self, segment):
         for i in range(segment.scope_count):
             scope = self._parsed_exe.scope_records[segment.scope_index - 1 + i]
-            self._apply_tdinfo_scope(segment, scope)
+            self._apply_scope(segment, scope)
 
-    def _apply_tdinfo_scope(self, segment, scope):
+    def _apply_scope(self, segment, scope):
         scope_offset = scope.offset if scope.parent == 0 else self._parsed_exe.scope_records[scope.parent - 1].offset
         scope_ea = self._image_base + segment.code_segment * 0x10 + scope_offset
 
         for i in range(scope.symbol_count):
             symbol = self._parsed_exe.symbol_records[scope.symbol_index - 1 + i]
-            self._apply_tdinfo_localvar(symbol, segment, scope_ea, scope_offset)
+            self._apply_local_variable(symbol, segment, scope_ea, scope_offset)
 
-    def _apply_tdinfo_localvar(self, symbol, segment, scope_ea, scope_offset):
+    def _apply_local_variable(self, symbol, segment, scope_ea, scope_offset):
         if symbol.bitfield.symbol_class != tdinfo_structs.SymbolClass.AUTO.name:
             return
 
